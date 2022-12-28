76c2ee82c3265f00bf867a773c8b475ba987e25e
Matheus <mat.araujoribeiro@gmail.com>
2022-12-28 14:18:01 -0300

    add closing modal logic to the key 'Escape'

--- script.js
+++ script.js
@@ -8,18 +8,24 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", function () {
-    console.log("button clicked!");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
-}
-
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 const closeModal = () => {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
 
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    if (!modal.classList.contains("hidden")) closeModal();
+  }
+});
